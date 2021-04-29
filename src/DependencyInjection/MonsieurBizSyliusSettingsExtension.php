b3bfccb0d0ee9a9aa30286073efb8b01bd57a525
clemence pereira <c.pereira@monsieurbiz.com>
2021-04-29 17:43:25 +0200

    Change doctrine config

--- src/DependencyInjection/MonsieurBizSyliusSettingsExtension.php
+++ src/DependencyInjection/MonsieurBizSyliusSettingsExtension.php
@@ -16,9 +16,10 @@ namespace MonsieurBiz\SyliusSettingsPlugin\DependencyInjection;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
-final class MonsieurBizSyliusSettingsExtension extends Extension
+final class MonsieurBizSyliusSettingsExtension extends Extension implements PrependExtensionInterface
 {
     /**
      * {@inheritdoc}
@@ -36,5 +37,18 @@ final class MonsieurBizSyliusSettingsExtension extends Extension
     public function getAlias()
     {
         return str_replace('monsieur_biz', 'monsieurbiz', parent::getAlias());
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function prepend(ContainerBuilder $container): void
+    {
+        $doctrineConfig = $container->getExtensionConfig('doctrine_migrations');
+        $container->prependExtensionConfig('doctrine_migrations', [
+            'migrations_paths' => array_merge(array_pop($doctrineConfig)['migrations_paths'] ?? [], [
+                'MonsieurBiz\SyliusSettingsPlugin\Migrations' => '@MonsieurBizSyliusSettingsPlugin/Migrations',
+            ]),
+        ]);
     }
 }
