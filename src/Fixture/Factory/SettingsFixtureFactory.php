2dd60326c35faa71a632fa8d7adf2f8d7df558b8
Etienne Gutbub <e.gutbub@monsieurbiz.com>
2023-07-24 11:00:24 +0200

    feat(fixture): Add fixture factory

--- src/Fixture/Factory/SettingsFixtureFactory.php
+++ src/Fixture/Factory/SettingsFixtureFactory.php
@@ -13,31 +13,67 @@ declare(strict_types=1);
 
 namespace MonsieurBiz\SyliusSettingsPlugin\Fixture\Factory;
 
+use MonsieurBiz\SyliusSettingsPlugin\Entity\Setting\SettingInterface;
+use MonsieurBiz\SyliusSettingsPlugin\Settings\RegistryInterface;
 use Sylius\Bundle\CoreBundle\Fixture\Factory\AbstractExampleFactory;
+use Sylius\Component\Channel\Repository\ChannelRepositoryInterface;
+use Sylius\Component\Core\Model\ChannelInterface;
 use Sylius\Component\Resource\Factory\FactoryInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
+
 class SettingsFixtureFactory extends AbstractExampleFactory
 {
-    private FactoryInterface $settingsFactory;
+    private RegistryInterface $settingsRegistry;
 
     private OptionsResolver $optionsResolver;
 
+    private ChannelRepositoryInterface $channelRepository;
+
     public function __construct(
-        FactoryInterface $settingsFactory,
+        RegistryInterface $settingsRegistry,
+        ChannelRepositoryInterface $channelRepository
     )
     {
-        $this->settingsFactory = $settingsFactory;
+        $this->settingsRegistry = $settingsRegistry;
+        $this->channelRepository = $channelRepository;
         $this->optionsResolver = new OptionsResolver();
+    }
+
+    public function create(array $options = []): SettingInterface
+    {
+        $options = $this->optionsResolver->resolve($options);
+
+        /** @var SettingInterface $settings */
+        $settings = $this->settingsRegistry->getByAlias($options['alias']);
+        $settings->setPath($options['path']);
+        $settings->setStorageType($options['type']);
+        $settings->setValue($options['value']);
+        $settings->setLocaleCode($options['locale']);
+
+        if (null !== $options['channel']) {
+            /** @var ?ChannelInterface $channel */
+            $channel = $this->channelRepository->findOneBy(['code' => $options['channel']]);
+            $settings->setChannel($channel);
+        }
+
+        return $settings;
     }
 
     protected function configureOptions(OptionsResolver $resolver): void
     {
-        // TODO: Implement configureOptions() method.
-    }
-
-    public function create(array $options = [])
-    {
-        // TODO: Implement create() method.
+        $resolver
+            ->setDefault('alias', '')
+            ->setAllowedTypes('alias', 'string')
+            ->setDefault('path', '')
+            ->setAllowedTypes('path', 'string')
+            ->setDefault('channel', null)
+            ->setAllowedTypes('channel', ['null', 'string'])
+            ->setDefault('locale', null)
+            ->setAllowedTypes('locale', ['null', 'string'])
+            ->setDefault('type', 'text')
+            ->setAllowedTypes('type', 'string')
+            ->setDefault('value', null)
+        ;
     }
 }
