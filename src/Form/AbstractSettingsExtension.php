2e8034b78b6b12b4a59ec400d7f8ad0beea8c072
Maxime Huran <m.huran@monsieurbiz.com>
2023-10-09 17:15:42 +0200

    Add classes to be able to extends a setting form type

---
+++ src/Form/AbstractSettingsExtension.php
@@ -0,0 +1,42 @@
+<?php
+
+/*
+ * This file is part of Monsieur Biz' Settings plugin for Sylius.
+ *
+ * (c) Monsieur Biz <sylius@monsieurbiz.com>
+ *
+ * For the full copyright and license information, please view the LICENSE.txt
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace MonsieurBiz\SyliusSettingsPlugin\Form;
+
+use MonsieurBiz\SyliusSettingsPlugin\Settings\Settings;
+use Symfony\Component\Form\AbstractTypeExtension;
+use Symfony\Component\Form\FormBuilderInterface;
+
+abstract class AbstractSettingsExtension extends AbstractTypeExtension implements SettingsExtensionInterface
+{
+    public function addWithDefaultCheckbox(FormBuilderInterface $builder, string $child, string $type = null, array $options = []): self
+    {
+        $data = (array) $builder->getData();
+        $builder->add($child, $type, $options);
+        if (!$this->isDefaultForm($builder)) {
+            $builder->add($child . '___' . Settings::DEFAULT_KEY, DefaultCheckboxType::class, [
+                'label' => 'monsieurbiz.settings.ui.use_default_value',
+                'related_form_child' => $builder->get($child),
+                'data' => !\array_key_exists($child, $data),
+                'required' => true,
+            ]);
+        }
+
+        return $this;
+    }
+
+    protected function isDefaultForm(FormBuilderInterface $builder): bool
+    {
+        return !$builder->getOption('show_default_checkboxes', true);
+    }
+}
